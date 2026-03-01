dc53356ee3b15b2b19dbbe6517e29d07d254f93f
kezapa <kezia@kabarac.ac.ke>
2026-03-01 16:01:47 +0300

    Fix budgets and environment configs

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: true,
-    apiUrl:'https://trackmint-backend-production.up.railway.app/api'
+  production: false,
+  apiUrl: 'http://localhost:8080/api'
 };
